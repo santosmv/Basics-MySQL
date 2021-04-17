28330df263fc8b71504c35c9368d820ef16a7157
mvsantos001 <mvsantos@ifi.unicamp.br>
2021-04-17 11:33:57 -0300

    Including MySQL in one folder
    Including MySQL in one folder and start another folder to use database in Python

---
+++ Sqlite3/database.py
@@ -0,0 +1,16 @@
+import sqlite3
+conn = sqlite3.connect('customer.db')
+#create a cursor
+cursor = conn.cursor()
+#create a table
+cursor.execute("""CREATE TABLE customers(
+    first_name text,
+    last_name text,
+    email text
+)
+""")
+
+conn.commit()
+
+#close
+conn.close()
