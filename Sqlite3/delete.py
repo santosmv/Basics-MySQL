647a4f445b25f56a25376158fd6609b650661d79
mvsantos001 <mvsantos@ifi.unicamp.br>
2021-04-21 19:03:34 -0300

    Including count and the use of %s

--- Sqlite3/delete.py
+++ Sqlite3/delete.py
@@ -6,6 +6,7 @@ cursor = conn.cursor()
 choose_id = 4
 
 cursor.execute("delete from customers where rowid = %d"%choose_id)
+#cursor.execute("delete from best_fit_results where id = '%s'"%phase_back_mix)
 
 cursor.execute("select rowid, * from customers")
 items = cursor.fetchall()
--- Sqlite3/select.py
+++ Sqlite3/select.py
@@ -5,6 +5,11 @@ cursor = conn.cursor()
 
 #select all elements in the database
 cursor.execute("SELECT * FROM customers")
+
+#count number of specific elements
+#name = 'marcos'
+#cursor.execute("select count(*) from customers where first_name = '%s'"%name)
+
 #show the elements
 #print(cursor.fetchone())
 #print(cursor.fetchmany(3))
