fea35b097b297893dc363a3d902091f9d180050f
mvsantos001 <mvsantos@ifi.unicamp.br>
2024-02-13 13:01:01 -0300

    Inclusion of pySpark

---
+++ pySpark/installing.sh
@@ -0,0 +1,14 @@
+# Install java jdk
+java --version
+# Install Spark from spark.apache.org/downloads.html
+
+# DO NOT CREATE AN ENVIRONMENT---------
+# Create an environment
+# python -m venv .pyspark-env
+# Activate the environment
+# .pyspark-env\Scripts\activate--------
+
+# Install pyspark
+pip install pyspark
+# Install findspark
+pip install findspark
